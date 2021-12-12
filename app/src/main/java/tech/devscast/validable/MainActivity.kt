bf81398892618b42978733cebe8b1b95954c0487
YvesKalume <19kn132@esisalama.org>
2021-12-12 21:06:52 +0200

    add sample project

--- app/src/main/java/tech/devscast/validable/MainActivity.kt
+++ app/src/main/java/tech/devscast/validable/MainActivity.kt
@@ -1,35 +1,24 @@
 package tech.devscast.validable
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import tech.devscast.validable.lib.InputContent
-import tech.devscast.validable.lib.validable
+import tech.devscast.validable.ui.screens.InputScreen
 import tech.devscast.validable.ui.theme.ValidableTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ValidableTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    FormSection()
+                    InputScreen()
                 }
             }
         }
@@ -46,39 +35,5 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ValidableTheme {
         Greeting("Android")
-    }
-}
-
-
-@Composable
-fun FormSection() {
-    var text by validable(initialValue = "yv", rules = {
-        isNotEmpty()
-    })
-
-
-
-    val context = LocalContext.current
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        OutlinedTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp)
-                .align(Alignment.Center),
-            value = text.value,
-            onValueChange = { text = text.withNewValue(it) },
-            isError = text.errors.isNotEmpty(),
-
-            )
-        Button(onClick = {
-            text.submit {
-                Toast.makeText(context,it,Toast.LENGTH_SHORT).show()
-                Toast.makeText(context,text.errors.isEmpty().toString(),Toast.LENGTH_SHORT).show()
-            }
-
-        }) {
-            Text(text = "Submit")
-        }
     }
 }
---
+++ app/src/main/java/tech/devscast/validable/ui/screens/InputScreen.kt
@@ -0,0 +1,88 @@
+package tech.devscast.validable.ui.screens
+
+import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import tech.devscast.validable.NotEmptyValidable
+import tech.devscast.validable.delegates.validableEmail
+import tech.devscast.validable.withValidable
+
+
+@ExperimentalAnimationApi
+@Composable
+fun InputScreen() {
+
+    val context = LocalContext.current
+
+    val emailField by validableEmail()
+    val nameField = remember { NotEmptyValidable() }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.padding(24.dp)
+    ) {
+
+        TextField(
+            value = emailField.value,
+            onValueChange = { emailField.value = it },
+            isError = emailField.hasError(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        AnimatedVisibility(visible = emailField.hasError()) {
+            TextFieldError(textError = emailField.errorMessage ?: "")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = nameField.value,
+            onValueChange = { nameField.value = it },
+            isError = nameField.hasError(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        AnimatedVisibility(visible = nameField.hasError()) {
+            TextFieldError(textError = nameField.errorMessage ?: "")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+
+            withValidable(emailField,nameField) {
+                Toast.makeText(context,"All fields are valid",Toast.LENGTH_SHORT).show()
+            }
+
+        }) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+fun TextFieldError(textError: String) {
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = textError,
+            modifier = Modifier.fillMaxWidth(),
+            style = LocalTextStyle.current.copy(color = MaterialTheme.colors.error)
+        )
+    }
+}
+
+@Preview
+@ExperimentalAnimationApi
+@Composable
+fun InputScreenPreview() {
+    InputScreen()
+}
