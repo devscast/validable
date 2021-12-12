c7ef7e0959d530914fd999666d3df26ec0ea343c
YvesKalume <19kn132@esisalama.org>
2021-12-12 21:05:30 +0200

    add withValidable

---
+++ validable/src/main/java/tech/devscast/validable/withValidable.kt
@@ -0,0 +1,13 @@
+package tech.devscast.validable
+
+fun withValidable(vararg fields : BaseValidable, next: () -> Unit) {
+    val errors : List<Boolean> = fields.map {
+        it.enableShowErrors()
+        it.hasError()
+    }
+
+    if(!errors.contains(true)) {
+        next()
+    }
+
+}
