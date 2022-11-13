56204e0aa4c297514433b4488c5df7feecf6c2ec
yveskalume <yvkalume@gmail.com>
2022-11-13 10:05:07 +0200

    use string Patterns for email

--- validable/src/main/java/tech/devscast/validable/EmailValidable.kt
+++ validable/src/main/java/tech/devscast/validable/EmailValidable.kt
@@ -1,14 +1,16 @@
 package tech.devscast.validable
 
+import java.util.regex.Pattern
+
 class EmailValidable : BaseValidable(validator = ::isEmailValid, errorFor = ::emailValidationError)
 
 private fun isEmailValid(email: String): Boolean {
-	return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    return Pattern.matches("^[A-Za-z](.*)([@]{1})(.+)(\\.)(.+)", email)
 }
 
 /**
  * Returns an error to be displayed or null if no error was found
  */
 private fun emailValidationError(email: String): String {
-	return "Invalid email: $email"
+    return "Invalid email: $email"
 }
