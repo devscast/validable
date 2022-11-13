bf81c3858779b62b7d62308a815225ad4346a6f1
yveskalume <yvkalume@gmail.com>
2022-11-13 08:54:30 +0200

    fix builds issues

--- sample/src/main/java/tech/devscast/validable_sample/MainActivity.kt
+++ sample/src/main/java/tech/devscast/validable_sample/MainActivity.kt
@@ -6,23 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import tech.devscast.validable_sample.ui.screens.InputScreen
 import tech.devscast.validable_sample.ui.theme.ValidableTheme
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalAnimationApi
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ValidableTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    InputScreen()
-                }
-            }
-        }
-    }
+	@ExperimentalAnimationApi
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			ValidableTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(color = MaterialTheme.colors.background) {
+					InputScreen()
+				}
+			}
+		}
+	}
 }
