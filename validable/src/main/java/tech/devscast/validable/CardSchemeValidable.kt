a7717bee348600c8fd63e5bef55f893f57bfe90c
Yves Kalume <55670723+yveskalume@users.noreply.github.com>
2024-05-01 12:13:25 +0300

    Fix cardschemevalidable (#33)
    * update : varag CardScheme on CardSchemeValidable
    
    * fix : CardSchemeValidable regex
    
    * fix : Suppress "LongMethod error" for getValidCardNumbers and getValidCardMap in tests
    
    * dump public Api

--- validable/src/main/java/tech/devscast/validable/CardSchemeValidable.kt
+++ validable/src/main/java/tech/devscast/validable/CardSchemeValidable.kt
@@ -5,9 +5,12 @@ import java.util.regex.Pattern
 /**
  * Validates that a card number belongs to a specified scheme.
  */
-class CardSchemeValidable(scheme: CardScheme, message: String? = null) : BaseValidable(
+class CardSchemeValidable(vararg schemes: CardScheme, message: String? = null) : BaseValidable(
     validator = { value ->
-        scheme.patterns.any { pattern -> Pattern.matches(pattern, value) }
+        schemes.any { scheme ->
+            val compiledPatterns = scheme.patterns.map { Pattern.compile(it) }
+            compiledPatterns.any { pattern -> pattern.matcher(value).matches() }
+        }
     },
     errorFor = { _ -> message ?: "Unsupported card type or invalid card number." }
 )
@@ -16,7 +19,7 @@ class CardSchemeValidable(scheme: CardScheme, message: String? = null) : BaseVal
  * see https://en.wikipedia.org/wiki/Payment_card_number
  * see https://www.regular-expressions.info/creditcard.html
  */
-open class CardScheme(val patterns: List<String>) {
+open class CardScheme protected constructor(val patterns: List<String>) {
 
     companion object {
         fun merge(vararg schemes: CardScheme): CardScheme {
@@ -28,7 +31,7 @@ open class CardScheme(val patterns: List<String>) {
      * American Express card numbers start with 34 or 37 and have 15 digits.
      */
     data object Amex : CardScheme(
-        listOf("/^3[47][0-9]{13}\$/")
+        listOf("^3[47][0-9]{13}\$")
     )
 
     /**
@@ -36,7 +39,7 @@ open class CardScheme(val patterns: List<String>) {
      * Please note that these cards do not follow Luhn Algorithm as a checksum.
      */
     data object ChinaUnionPay : CardScheme(
-        listOf("/^62[0-9]{14,17}\$/")
+        listOf("^62[0-9]{14,17}\$")
     )
 
     /**
@@ -45,7 +48,7 @@ open class CardScheme(val patterns: List<String>) {
      * These are a joint venture between Diners Club and MasterCard, and should be processed like a MasterCard.
      */
     data object Diners : CardScheme(
-        listOf("/^3(?:0[0-5]|[68][0-9])[0-9]{11}\$/")
+        listOf("^3(?:0[0-5]|[68][0-9])[0-9]{11}\$")
     )
 
     /**
@@ -54,10 +57,10 @@ open class CardScheme(val patterns: List<String>) {
      */
     data object Discover : CardScheme(
         listOf(
-            "/^6011[0-9]{12}$/",
-            "/^64[4-9][0-9]{13}$/",
-            "/^65[0-9]{14}$/",
-            "/^622(12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|91[0-9]|92[0-5])[0-9]{10}$/"
+            "^6011[0-9]{12}$",
+            "^64[4-9][0-9]{13}$",
+            "^65[0-9]{14}$",
+            "^622(12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|91[0-9]|92[0-5])[0-9]{10}$"
         )
     )
 
@@ -65,7 +68,7 @@ open class CardScheme(val patterns: List<String>) {
      * InstaPayment cards begin with 637 through 639 and have 16 digits.
      */
     data object InstaPayment : CardScheme(
-        listOf("/^63[7-9][0-9]{13}\$/")
+        listOf("^63[7-9][0-9]{13}\$")
     )
 
     /**
@@ -73,14 +76,14 @@ open class CardScheme(val patterns: List<String>) {
      * JCB cards beginning with 35 have 16 digits.
      */
     data object Jcb : CardScheme(
-        listOf("/^(?:2131|1800|35[0-9]{3})[0-9]{11}\$/")
+        listOf("^(?:2131|1800|35[0-9]{3})[0-9]{11}\$")
     )
 
     /**
      * Laser cards begin with either 6304, 6706, 6709 or 6771 and have between 16 and 19 digits.
      */
     data object Laser : CardScheme(
-        listOf("/^(6304|670[69]|6771)[0-9]{12,15}\$/")
+        listOf("^(6304|670[69]|6771)[0-9]{12,15}\$")
     )
 
     /**
@@ -89,10 +92,10 @@ open class CardScheme(val patterns: List<String>) {
      */
     data object Maestro : CardScheme(
         listOf(
-            "/^(6759[0-9]{2})[0-9]{6,13}$/",
-            "/^(50[0-9]{4})[0-9]{6,13}$/",
-            "/^5[6-9][0-9]{10,17}$/",
-            "/^6[0-9]{11,18}$/"
+            "^(6759[0-9]{2})[0-9]{6,13}$",
+            "^(50[0-9]{4})[0-9]{6,13}$",
+            "^5[6-9][0-9]{10,17}$",
+            "^6[0-9]{11,18}$"
         )
     )
 
@@ -102,8 +105,8 @@ open class CardScheme(val patterns: List<String>) {
      */
     data object MasterCard : CardScheme(
         listOf(
-            "/^5[1-5][0-9]{14}$/",
-            "/^2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12})$/"
+            "^5[1-5][0-9]{14}$",
+            "^2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12})$"
         )
     )
 
@@ -111,20 +114,20 @@ open class CardScheme(val patterns: List<String>) {
      * Payment system MIR numbers start with 220, then 1 digit from 0 to 4, then between 12 and 15 digits
      */
     data object Mir : CardScheme(
-        listOf("/^220[0-4][0-9]{12,15}\$/")
+        listOf("^220[0-4][0-9]{12,15}\$")
     )
 
     /**
      * All UATP card numbers start with a 1 and have a length of 15 digits.
      */
     data object Uatp : CardScheme(
-        listOf("/^1[0-9]{14}\$/")
+        listOf("^1[0-9]{14}\$")
     )
 
     /**
      * All Visa card numbers start with a 4 and have a length of 13, 16, or 19 digits.
      */
     data object Visa : CardScheme(
-        listOf("/^4([0-9]{12}|[0-9]{15}|[0-9]{18})\$/")
+        listOf("^4([0-9]{12}|[0-9]{15}|[0-9]{18})\$")
     )
 }
